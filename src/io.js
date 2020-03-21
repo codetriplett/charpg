f07e2c2df703ce1133dabcc2300e2f41946b1645
unknown <code.triplett@gmail.com>
2020-03-21 18:43:22 -0500

    added mongodb

--- src/io.js
+++ src/io.js
@@ -6,19 +6,16 @@ export function io (x, z, y, chunk) {
 	const name = [x, z, y].join('_');
 
 	if (chunk) {
-		localStorage.setItem(name, JSON.stringify(chunk));
-	} else if (chunks.hasOwnProperty(name)) {
-		chunk = chunks[name];
-	} else if (localStorage.hasOwnProperty(name)) {
-		chunk = JSON.parse(localStorage.getItem(name));
-	} else if (name === '0_0_0') {
-		return fetch(`/${name}.json`)
+		return fetch(`/tiles/${name}.json`, {
+			method: 'POST',
+			body: JSON.stringify(chunk)
+		});
+	} else if (!chunks.hasOwnProperty(name)) {
+		return fetch(`/tiles/${name}.json`)
 			.then(response => response.json())
 			.catch(() => inflate([Array(9).fill(Array(10).join(' '))], 9))
 			.then(chunk => chunks[name] = chunk);
-	} else {
-		chunk = inflate([Array(9).fill(Array(10).join(' '))], 9);
 	}
 
-	return Promise.resolve(chunks[name] = chunk);
+	return Promise.resolve(chunks[name]);
 }
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const { MongoClient } = require('mongodb');
 
 const types = {
 	txt: 'text/html; charset=utf-8',
@@ -14,6 +15,22 @@ const types = {
 	png: 'image/png',
 	ico: 'image/x-icon'
 };
+
+function capture (req) {
+	return new Promise(resolve => {
+		let body = '';
+		
+		req.on('data', (data) => {
+			body += data;
+
+			if (body.length > 1e6) {
+				req.connection.destroy();
+			}
+		});
+
+		req.on('end', () => resolve(body));
+	});
+}
 
 function respond (res, content, type) {
 	let status = 200;
@@ -33,17 +50,97 @@ function respond (res, content, type) {
 	res.end(content);
 }
 
-export default function (port, directory) {
-	http.createServer(({ url }, res) => {
+function store (database, folder, name, value) {
+	return MongoClient.connect(database).catch(() => {}).then(client => {
+		if (!client) {
+			return;
+		}
+
+		const index = database.lastIndexOf('/');
+		const db = client.db(database.slice(index + 1));
+
+		if (db) {
+			const collection = db.collection(folder);
+
+			if (collection) {
+				return collection.findOne({ name }).then(document => {
+					if (!document) {
+						return collection.insertOne({ name, value });
+					}
+
+					return collection.updateOne({ name }, {
+						$set: { name, value }
+					});
+				}).then(() => {
+					client.close();
+					return 'Tile Saved';
+				});
+			}
+		}
+
+		client.close();
+		return Promise.resolve();
+	});
+}
+
+function retrieve (database, folder, name) {
+	return MongoClient.connect(database).catch(() => {}).then(client => {
+		if (!client) {
+			return;
+		}
+
+		const index = database.lastIndexOf('/');
+		const db = client.db(database.slice(index + 1));
+		let tile;
+
+		if (db) {
+			const collection = db.collection(folder);
+
+			if (collection) {
+				tile = collection.findOne({ name });
+			}
+		}
+
+		client.close();
+		return tile;
+	});
+}
+
+export default function (port, directory, database) {
+	http.createServer((req, res) => {
+		let { url, method } = req;
 		let extension = (url.match(/^.*?(\.[^.]*)?$/)[1] || '').slice(1);
+		let recorder;
 	
-		if (url === '/') {
+		if (method.toLowerCase() === 'post') {
+			recorder = capture(req);
+		} else if (url === '/') {
 			url += 'index';
 		}
-	
-		if (!extension) {
-			extension = '.html';
-			url += extension;
+
+		if (recorder || extension === 'json') {
+			const index = url.lastIndexOf('/');
+			const folder = url.slice(1, index);
+			const name = url.slice(index + 1, -5);
+
+			if (!database && (!folder || !name)) {
+				respond(res);
+			} else if (recorder) {
+				recorder.then(body => {
+					store(database, folder, name, body).then(message => {
+						respond(res, message);
+					});
+				});
+			} else {
+				retrieve(database, folder, name).then(data => {
+					respond(res, data ? data.value : undefined, extension);
+				});
+			}
+
+			return;
+		} else if (!extension) {
+			extension = 'html';
+			url += `.${extension}`;
 		}
 	
 		const type = types[extension] || '';
